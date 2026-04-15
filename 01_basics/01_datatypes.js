faa0c83f864a34a957e4de1a63b41c6a863b32ad
Deepak Prajapati <deepak1503prajapati@gmail.com>
2026-04-15 07:18:48 +0000

    datatype completed

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //---> for use modern methods 
+
+// alert(3+3) because we are not using browser we are using codespace .
+
+// we should maintain  code readability 
+console.log(3
+    +3)
+// data types
+
+let name = "Deepak"
+let age = 21
+let isLoggedin = false
+let state = null
+
+//name = string 
+// age = integer
+// bigint
+// typeof any data 
+console.log(typeof 21);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
