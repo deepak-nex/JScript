05ed50551d60d549d3d106c18803e81140d96040
Deepak Prajapati <deepak1503prajapati@gmail.com>
2026-04-15 07:07:56 +0000

    variable completed

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = ("02081073");
+// we can not change const value in js 
+//accountId = "02081028";
+let accountEmail = "Jonny143@gmail.com";
+var accountPassword = "1432";
+accountCity = "jabalpur";
+let accountState;
+/*
+We will never use var 
+because it is not good in block scope and function scope 
+*/
+console.table([accountId ,accountEmail,accountPassword,accountCity,accountState]);
